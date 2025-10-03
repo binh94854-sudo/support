155d6f5efd9ee7482e89b893725c187f197828f2
binh94854-sudo <binh94854@gmail.com>
2025-10-03 15:04:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const keepAlive = require("./keepAlive");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 KeepAlive server running on port ${PORT}`);
+});
+
 const { 
   Client, 
   GatewayIntentBits, 
@@ -152,5 +163,4 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 // ========== LOGIN ==========
-keepAlive();
 client.login(process.env.DISCORD_TOKEN);
